108a80917e1cc60857db259d51fda4eef842cf0a
Osievskaya23 <valentinaosievskaya@gmail.com>
2019-04-04 08:32:34 +0300

    hashing utils added

--- main/java/com/vosievskaya/util/Util.java
+++ main/java/com/vosievskaya/util/Util.java
@@ -1,4 +1,30 @@
 package com.vosievskaya.util;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.UUID;
+
 public class Util {
+
+    public static String sha256(String base) {
+        try{
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(base.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder();
+
+            for (byte hash1 : hash) {
+                String hex = Integer.toHexString(0xff & hash1);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch(Exception ex){
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static String generateToken() {
+        return UUID.randomUUID().toString();
+    }
 }
